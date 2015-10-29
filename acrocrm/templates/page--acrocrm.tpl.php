860a262618e079c0b785e1983cc2004ddccc0160
Zach Holland <zacharias.holland@gmail.com>
2015-10-29 12:47:43 -0700

    Attempted to create a template for our module. This is what I have so far. It doesn't really work how I expected.
    page--acrocrm.tpl.php is the template.
    
    page--acrocrm.tpl.php.bak is just a copy of the sb-admin index.html for reference.

---
+++ acrocrm/templates/page--acrocrm.tpl.php
@@ -0,0 +1,27 @@
+<div id="wrapper">
+  <!-- Navigation -->
+  <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="index.html">SB Admin v2.0</a>
+    </div>
+    <!-- /.navbar-header -->
+
+    <div class="navbar-default sidebar" role="navigation">
+      <div class="sidebar-nav navbar-collapse">
+        <?php if ($page['sidebar_first']): ?>
+          <ul class="nav" id="side-menu">
+            <?php print render($page['sidebar_first']); ?>
+          </ul>
+        <?php else: ?>
+          <p>There are no modules installed.</p>
+        <?php endif; ?>
+      </div>
+      <!-- /.sidebar-collapse -->
+    </div>
+    <!-- /.navbar-static-side -->
+  </nav>
+  <div id="page-wrapper">
+    <?php print render($page['content']); ?>
+  </div>
+  <!-- /#page-wrapper -->
+</div>
