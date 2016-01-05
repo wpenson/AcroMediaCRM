e1a26753aa66655cc15170502d4e49e054a99426
Wade Penson <wadepenson@gmail.com>
2016-01-05 13:25:36 -0800

    Got redirection to work for the form.

--- acrocrm/templates/assets/js/acrocrm.js
+++ acrocrm/templates/assets/js/acrocrm.js
@@ -32,5 +32,46 @@ function assignPriority(lead_id, element) {
     $.ajax({
         url: "/acrocrm_leads/assign_lead_priority/" + lead_id + "/" + $(element).val()
     });
-
 }
+
+//Drupal.behaviors.acrocrm_leads = {
+//    'attach': function(context) {
+//        function test_submit() {alert('test'); return false;
+//        }
+//
+//$("#acrocrm-leads-single-lead-form").submit(function(e) {
+//    console.log("test");
+//    alert("test2");
+//    e.preventDefault();
+//});
+//    }
+//}
+//
+//Drupal.behaviors.acrocrm_leads = {
+//    'attach': function(context) {
+//        $('#leadEditModal').on('show.bs.modal', function (e) {
+//            //if (!data) return e.preventDefault() // stops modal from being shown
+//            var button = $(event.relatedTarget);
+//            var lead_id = button.data('id');
+//
+//            var modal = $(this)
+//            $.get('/drupal/acrocrm_leads/edit_lead/' + parseInt(lead_id, 10), null, editLead);
+//            return false;
+//        })
+//    }
+//}
+//
+//var editLead = function(response) {
+//    //var result = Drupal.parseJson(response);
+//    //alert(response);
+//    $('#leadEditModal .modal-body').html(response.data);
+//}
+//Drupal.behaviors.acrocrm_leads = {
+//    'attach': function(context) {
+//        $('#leadEditModal').on('shown.bs.modal', function (e) {
+//            var id = $(e.relatedTarget).data('id');
+//            $(this).find('iframe').attr('src', '/drupal/acrocrm_leads/edit_lead/' + id);
+//            $(this).modal({show:true});
+//        })
+//    }
+//}
