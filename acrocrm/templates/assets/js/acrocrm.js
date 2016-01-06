802817409d9f6a228f4e641a1431dbd921666a5d
Wade Penson <wadepenson@gmail.com>
2016-01-06 14:57:00 -0800

    Edit and delete lead is now working.

--- acrocrm/templates/assets/js/acrocrm.js
+++ acrocrm/templates/assets/js/acrocrm.js
@@ -28,50 +28,37 @@ $( ".accordion" ).accordion({
 });
 
 function assignPriority(lead_id, element) {
-
     $.ajax({
         url: "/acrocrm_leads/assign_lead_priority/" + lead_id + "/" + $(element).val()
     });
 }
 
-//Drupal.behaviors.acrocrm_leads = {
-//    'attach': function(context) {
-//        function test_submit() {alert('test'); return false;
-//        }
-//
-//$("#acrocrm-leads-single-lead-form").submit(function(e) {
-//    console.log("test");
-//    alert("test2");
-//    e.preventDefault();
-//});
-//    }
-//}
-//
-//Drupal.behaviors.acrocrm_leads = {
-//    'attach': function(context) {
-//        $('#leadEditModal').on('show.bs.modal', function (e) {
-//            //if (!data) return e.preventDefault() // stops modal from being shown
-//            var button = $(event.relatedTarget);
-//            var lead_id = button.data('id');
-//
-//            var modal = $(this)
-//            $.get('/drupal/acrocrm_leads/edit_lead/' + parseInt(lead_id, 10), null, editLead);
-//            return false;
-//        })
-//    }
-//}
-//
-//var editLead = function(response) {
-//    //var result = Drupal.parseJson(response);
-//    //alert(response);
-//    $('#leadEditModal .modal-body').html(response.data);
-//}
-//Drupal.behaviors.acrocrm_leads = {
-//    'attach': function(context) {
-//        $('#leadEditModal').on('shown.bs.modal', function (e) {
-//            var id = $(e.relatedTarget).data('id');
-//            $(this).find('iframe').attr('src', '/drupal/acrocrm_leads/edit_lead/' + id);
-//            $(this).modal({show:true});
-//        })
-//    }
-//}
+// --- Leads Edit and Delete Modal --- //
+Drupal.behaviors.acrocrm_leads = {
+    'attach': function(context) {
+        $('#deleteLeadConfirmationModal').on('show.bs.modal', function (event) {
+            var trigger_button = $(event.relatedTarget);
+            var id = trigger_button.data('id'); // Extract info from data-* attributes
+            var url = trigger_button.data('url');
+
+            var modal = $(this);
+
+            //modal.find('#deleteLeadConfirmationButton').click(function() {
+            //    $.ajax({
+            //        url: url + id,
+            //    })
+            //    .done(function() {
+            //        //modal.modal('hide');
+            //        //window.location.assign('overview');
+            //    })
+            //    .fail(function() {
+            //        alert('error');
+            //    });
+            //});
+
+            modal.find('#deleteLeadConfirmationButton').click(function() {
+                window.location.replace(url + id);
+            });
+        });
+    }
+};
